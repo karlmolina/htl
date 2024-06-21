7a20277a75238518553f6f43bc3e63b79b8adf57
Karl Molina <karlgmolina@gmail.com>
2024-06-21 14:06:14 -0600

    Add TaggedTemplateParams to types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,15 +1,20 @@
-declare module 'htl' {
-    /** Renders the specified markup as an element, text node, or null as appropriate. */
-    const html: {
-        <T extends HTMLElement | Text | null>(...args: any[]): T;
-        /** Renders the specified markup as a document fragment. */
-        fragment(...args: any[]): DocumentFragment;
-    };
-    /** Renders the specified markup as an SVG element, text node, or null as appropriate. */
-    const svg: {
-        <T extends SVGElement | Text | null>(...args: any[]): T;
-        /** Renders the specified markup as a document fragment. */
-        fragment(...args: any[]): DocumentFragment;
-    };
-    export { html, svg };
+declare module "htl" {
+  /** Renders the specified markup as an element, text node, or null as appropriate. */
+  type TaggedTemplateParams = [
+    template: { raw: readonly string[] | ArrayLike<string> },
+    ...substitutions: any[],
+  ];
+  /** Renders the specified markup as an HTML element, text node, or null as appropriate. */
+  const html: {
+    <T extends HTMLElement | Text | null>(...args: TaggedTemplateParams): T;
+    /** Renders the specified markup as a document fragment. */
+    fragment(...args: TaggedTemplateParams): DocumentFragment;
+  };
+  /** Renders the specified markup as an SVG element, text node, or null as appropriate. */
+  const svg: {
+    <T extends SVGElement | Text | null>(...args: TaggedTemplateParams): T;
+    /** Renders the specified markup as a document fragment. */
+    fragment(...args: TaggedTemplateParams): DocumentFragment;
+  };
+  export { html, svg };
 }
